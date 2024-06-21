855687537b9596bd997790553bc511a535710d4b
Naresh3102 <naresh31122002@gmail.com>
2024-06-21 12:02:33 +0530

    Checkout submission

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -19,9 +19,16 @@ const Checkout = () => {
     userProgressCtx.hideCheckout();
   }
 
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    const fd = new FormData(event.target);
+    const customerData = Object.fromEntries(fd.entries());
+  }
+
   return (
     <Modal open={userProgressCtx.progress === "checkout"} onClose={handleClose}>
-      <form>
+      <form onSubmit={handleSubmit}>
         <h2>Ckeckout</h2>
         <p>Total Amount: {currencyFormatter.format(cartTotal)}</p>
         <Input label="Full Name" type="text" id="full-name" />
